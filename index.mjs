305c1d4cc8f13242d4c06e505ab58d5e078e7a3e
Maciej Czerwonka <czerwonkamaciej25@gmail.com>
2022-05-06 20:55:45 +0200

    swiper

---
+++ index.mjs
@@ -0,0 +1,28 @@
+import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js';
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 0,
+    effect: "coverflow",
+    a11y: true,
+  // autoplay: {
+  //   delay: 5000,
+  // },
+  loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 3,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 800,
+      depth: 200,
+      modifier: 1,
+      slideShadows: false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
