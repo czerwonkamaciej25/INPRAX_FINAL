d3670d937c97943c3c9be6138805a2f3665166e4
Maciej Czerwonka <czerwonkamaciej25@gmail.com>
2022-07-07 12:49:37 +0200

    poprawa wykresow

--- chart.js
+++ chart.js
@@ -2,29 +2,76 @@ const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'doughnut',
     data: {
-        labels: ['Red', 'Blue'],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19],
+            data: [12],
             backgroundColor: [
                 'rgb(28,198,160)',
-                'rgba(54, 162, 235, 0.2)',
             
             ],
             borderColor: [
                 'rgb(28,198,160)',
-                'rgba(54, 162, 235, 1)',
             ],
-            borderWidth: 1
-          
+            borderWidth: 20,
+            weight:0
         }]
         
     },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
 });
+
+// var ctx = document.getElementById('myChart2').getContext('2d');
+// var myChart2 = new Chart(ctx, {
+//     type: 'doughnut',
+//     data: {
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12],
+//             backgroundColor: [
+//                 'rgb(28,198,160)',
+            
+//             ],
+//             borderColor: [
+//                 'rgb(28,198,160)',
+//             ],
+//             borderWidth: 20,
+//             weight:0
+//         }]
+        
+//     },
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// });
+
+// var ctx = document.getElementById('myChart3').getContext('2d');
+// var myChart3 = new Chart(ctx, {
+//     type: 'doughnut',
+//     data: {
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12],
+//             backgroundColor: [
+//                 'rgb(28,198,160)',
+            
+//             ],
+//             borderColor: [
+//                 'rgb(28,198,160)',
+//             ],
+//             borderWidth: 20,
+//             weight:0
+//         }]
+        
+//     },
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// });
+
